2e0769577e1b1495a22b020316703908258c5aba
Shreyas Pangal <73640884+shreyaspangal@users.noreply.github.com>
2024-04-05 16:46:33 +0530

    add: Suggestions sections animation & scroll prev and next

---
+++ animations/home/suggestionsAnimations.ts
@@ -0,0 +1,33 @@
+export const prevBtnVariants = {
+  initial: {
+    x: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5 },
+  },
+  exit: {
+    x: -100,
+    opacity: 0,
+    transition: { delay: 0.05, duration: 1 },
+  },
+};
+
+export const nextBtnVariants = {
+  initial: {
+    x: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5 },
+  },
+  exit: {
+    x: 100,
+    opacity: 0,
+    transition: { delay: 0.05, duration: 1 },
+  },
+};
--- components/home-page/suggestions.tsx
+++ components/home-page/suggestions.tsx
@@ -1,10 +1,43 @@
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+"use client";
+import { Carousel, CarouselApi, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { BiSolidChevronRight } from "react-icons/bi";
+import { CgChevronLeft, CgChevronRight } from "react-icons/cg";
+import { AnimatePresence, motion } from "framer-motion";
+import React from "react";
+import { prevBtnVariants, nextBtnVariants } from "@/animations/home/suggestionsAnimations";
 
-export default function Suggestions({ title, slides, type = "horizontal" }: any) {
+export default function Suggestions({ title, slides: slidesInit, type = "horizontal" }: any) {
   const horizontalCardStyles = "md:basis-1/2 lg:basis-1/3 h-[15.75rem]";
   const verticalCardStyles = "min-[425px]:basis-1/2 md:basis-1/3 lg:basis-1/6 h-[15.75rem]";
   const CarouselItemStyles = type === "horizontal" ? horizontalCardStyles : verticalCardStyles;
+
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [slides, setSlides] = React.useState(slidesInit);
+  const [hovered, setHovered] = React.useState(false);
+  const [slidePrev, setSlidePrev] = React.useState(false);
+  const [slideNext, setSlideNext] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+    if (slidePrev) {
+      api.scrollPrev();
+      setSlidePrev(false);
+    }
+    if (slideNext) {
+      api.scrollNext();
+      setSlideNext(false);
+    }
+  }, [api, slidePrev, slideNext]);
+
+  function handlePrevBtn() {
+    setSlidePrev(true);
+  }
+
+  function handleNextBtn() {
+    setSlideNext(true);
+  }
 
   return (
     <section className="suggestions max-w-[1440px] mx-auto p-[1.5rem] pt-[3rem] xl:py-[2.88rem] xl:px-[4.38rem]">
@@ -15,8 +48,8 @@ export default function Suggestions({ title, slides, type = "horizontal" }: any)
           <BiSolidChevronRight className="text-[#D49941] w-[25px] h-[25px] object-cover" />
         </button>
       </div>
-      <div className="pt-[0.81rem]">
-        <Carousel>
+      <motion.div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="overflow-hidden pt-[0.81rem]">
+        <Carousel setApi={setApi} className="relative">
           <CarouselContent>
             {slides.map((slide: any, index: number) => (
               <CarouselItem key={slide.id} className={CarouselItemStyles}>
@@ -24,8 +57,44 @@ export default function Suggestions({ title, slides, type = "horizontal" }: any)
               </CarouselItem>
             ))}
           </CarouselContent>
+          <AnimatePresence>{hovered && <PreviousBtn handlePrevBtn={handlePrevBtn} />}</AnimatePresence>
+          <AnimatePresence>{hovered && <NextBtn handleNextBtn={handleNextBtn} />}</AnimatePresence>
         </Carousel>
-      </div>
+      </motion.div>
     </section>
+  );
+}
+
+function PreviousBtn({ handlePrevBtn }: any) {
+  return (
+    <motion.div
+      variants={prevBtnVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      key="prevBtn"
+      className="absolute top-0 left-0 w-[40px] md:w-[68px] h-full bg-gray-900/50 hover:bg-gray-900/75"
+    >
+      <button type="button" onClick={handlePrevBtn} className="h-full w-full flex items-center justify-center">
+        <CgChevronLeft className="text-[#fff] w-[50px] h-[50px]" />
+      </button>
+    </motion.div>
+  );
+}
+
+function NextBtn({ handleNextBtn }: any) {
+  return (
+    <motion.div
+      variants={nextBtnVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      key="NextBtn"
+      className="absolute top-0 right-0 w-[40px] md:w-[68px] h-full bg-gray-900/50 hover:bg-gray-900/75"
+    >
+      <button type="button" onClick={handleNextBtn} className="h-full w-full flex items-center justify-center">
+        <CgChevronRight className="text-[#fff] w-[50px] h-[50px]" />
+      </button>
+    </motion.div>
   );
 }

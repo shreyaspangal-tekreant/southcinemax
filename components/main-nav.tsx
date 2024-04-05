4fea12af7dcb115e28ea1dd32cdbe02486280d6c
Shreyas Pangal <73640884+shreyaspangal@users.noreply.github.com>
2024-04-05 19:24:08 +0530

    fix: nav for mobile screens

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -31,42 +31,29 @@ export default function MainNav() {
           </ul>
         </nav>
         <div className="flex items-center gap-7">
-            <div className="xl:hidden">
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <CgMenu className="w-[1.5rem] h-[1.8rem] text-[#ADADAD] cursor-pointer" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  {navLinks.map((link, index) => (
-                    <DropdownMenuItem key={index} className="cursor-pointer">
-                      {link.name}
-                    </DropdownMenuItem>
-                  ))}
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-            <div className="searchbar-block relative w-[7.69rem]">
-              <RiSearchLine className="absolute top-[6px] text-[#ADADAD] w-[0.91rem] h-[0.91rem]" />
-              <input className="border-0 outline-0 border-b-[1px] border-[#ADADAD] bg-transparent text-[#ADADAD] w-full text-[0.81rem] pl-[1.06rem] pb-[0.31rem]" placeholder="Search..." />
-            </div>
-            <div className="profile-icon hidden lg:block">
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <GoPerson className="w-[1.5rem] h-[1.8rem] text-[#ADADAD] cursor-pointer" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem className="cursor-pointer">Profile</DropdownMenuItem>
-                  <DropdownMenuItem className="cursor-pointer">Billing</DropdownMenuItem>
-                  <DropdownMenuItem className="cursor-pointer">Team</DropdownMenuItem>
-                  <DropdownMenuItem className="cursor-pointer">Subscription</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-            <button type="button" className="hidden lg:block rounded-[50px] bg-[#D49941] px-[1.75rem] py-[0.31rem] font-bold hover:opacity-75">
-              Contact
-            </button>
+          <CgMenu className="lg:hidden w-[1.5rem] h-[1.8rem] text-[#ADADAD] cursor-pointer" />
+          <div className="searchbar-block hidden lg:block relative w-[7.69rem]">
+            <RiSearchLine className="absolute top-[6px] text-[#ADADAD] w-[0.91rem] h-[0.91rem]" />
+            <input className="border-0 outline-0 border-b-[1px] border-[#ADADAD] bg-transparent text-[#ADADAD] w-full text-[0.81rem] pl-[1.06rem] pb-[0.31rem]" placeholder="Search..." />
+          </div>
+          <div className="profile-icon hidden lg:block">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <GoPerson className="w-[1.5rem] h-[1.8rem] text-[#ADADAD] cursor-pointer" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="cursor-pointer">Profile</DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">Billing</DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">Team</DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">Subscription</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <button type="button" className="hidden lg:block rounded-[50px] bg-[#D49941] px-[1.75rem] py-[0.31rem] font-bold hover:opacity-75">
+            Contact
+          </button>
         </div>
       </div>
     </header>
